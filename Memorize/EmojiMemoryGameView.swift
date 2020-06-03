90f2650a41380663e0e425ca5501ec694c058195
sxc <qiuyequ@icloud.com>
2020-06-03 09:47:26 +0800

    add a circle background

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -44,6 +44,8 @@ struct CardView: View {
             if self.card.isFaceUp {
                 RoundedRectangle(cornerRadius: conerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: conerRadius).stroke(lineWidth: edgeLineWidth)
+//                Circle().padding(5).opacity(0.4)
+                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true).padding(5).opacity(0.4)
                 Text(self.card.content)
             } else {
                 if !card.isMatched {
@@ -58,13 +60,16 @@ struct CardView: View {
     let conerRadius: CGFloat = 10.0
     let edgeLineWidth: CGFloat = 3
     func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+        let game  = EmojiMemoryGame()
+        game.choose(card: game.cards[1])
+        
+        return EmojiMemoryGameView(viewModel: game)
     }
 }
