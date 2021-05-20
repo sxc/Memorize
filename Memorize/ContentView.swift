933f3e6d0df821e6b602bab26cd93eaaedfcdd8d
sxc <qiuyequ@icloud.com>
2021-05-20 13:39:12 +0800

    change card view

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,17 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = true
+    
+    var body: some View {
         ZStack {
-            
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-                .padding(.horizontal)
-                .foregroundColor(.red)
-            
-            
-            Text("Hello, world!")
-                .foregroundColor(.orange)
-                .padding()
+            let shape =  RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill()
+                    .foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("✈️")
+                    .font(.largeTitle)
+            } else {
+                shape.fill()
+            }
         }
     }
 }
@@ -27,5 +42,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
