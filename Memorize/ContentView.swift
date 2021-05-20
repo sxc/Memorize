42d0bb7ae3cf66bedd21f10ea381e3fa5af6d1d7
sxc <qiuyequ@icloud.com>
2021-05-20 14:58:38 +0800

    layout adaptive

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,16 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var emojis = ["🚗","🚐","🚛","🏎","🚡","🚉","🚀","💺","🚁","⚓️","🚤","🚨","🚂","🚝","🦽","🛴","🛳","🚍","🚋","🚲","","🚎","🛰","🚥","🚧"]
-    @State var emojiCount = 24
+    @State var emojiCount = 18
     
     var body: some View {
         VStack {
-            LazyVGrid(columns: [GridItem(),GridItem(),GridItem()]) {
-                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                    CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
                 }
+                .foregroundColor(.red)
             }
-            .foregroundColor(.red)
             Spacer()
 //            Spacer(minLength: 20)
             HStack {
@@ -64,7 +66,7 @@ struct CardView: View {
             if isFaceUp {
                 shape.fill()
                     .foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content)
                     .font(.largeTitle)
             } else {
