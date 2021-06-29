50b863146e9a0e2f7c217e596b1d248be87d2e78
sxc <qiuyequ@icloud.com>
2021-06-29 11:48:22 +0800

    rebuild file for Xcode 13

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Xiaochun Shen on 2021/6/29.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
